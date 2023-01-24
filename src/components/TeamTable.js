a0f9ef8372408110b86687e1bebcdb9cdaf872ac
Harigovind <harigovind@Harigovinds-MacBook-Pro.local>
2023-01-25 00:36:14 +0530

    changes teamscreen to new UI

--- src/components/TeamTable.js
+++ src/components/TeamTable.js
@@ -1,53 +1,56 @@
-import React, { useEffect, useState,useRef } from "react";
-import { Container, Row, Col, Table } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import {
+  MDBBadge,
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody,
+} from "mdb-react-ui-kit";
+import { Container, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { listEmployees } from "../actions/employeeActions";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Loader from "./Loader";
 import Message from "./Message";
+const baseURL = process.env.REACT_APP_BACKEND_BASEURL;
 
 function TeamTable() {
-  
   let [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
-  const [employeelist,setEmployeeList] = useState([])
-  
-  const tbodyRef = useRef(null);
-  const [scrollPos, setScrollPos] = useState(0);
-  
+  const [employeelist, setEmployeeList] = useState([]);
+
   const dispatch = useDispatch();
   const employeeList = useSelector((state) => state.employeeList);
   const { error, loading, employees } = employeeList;
-  const employee_array = employees && employees['results'] ? employees["results"] : null;
-  const totalEmployeeCount = employees && employees['count'] ? employees["count"] : null;
-  
-  if (employee_array && totalEmployeeCount && employeelist.length < totalEmployeeCount){
-    console.log(totalEmployeeCount)
-    employeelist.push(...employee_array)
+  const employee_array =
+    employees && employees["results"] ? employees["results"] : null;
+  const totalEmployeeCount =
+    employees && employees["count"] ? employees["count"] : null;
+
+  if (
+    employee_array &&
+    totalEmployeeCount &&
+    employeelist.length < totalEmployeeCount
+  ) {
+    console.log(totalEmployeeCount);
+    employeelist.push(...employee_array);
   }
 
   useEffect(() => {
-    setEmployeeList([])
+    setEmployeeList([]);
     dispatch(listEmployees(page));
   }, [dispatch]);
 
   const fetchData = () => {
-    setScrollPos(tbodyRef.current.scrollTop);
-    var next_page = employees['next']
-    if (!next_page){
+    var next_page = employees["next"];
+    if (!next_page) {
       setHasMore(false);
-    }
-    else{
+    } else {
       setPage(page + 1);
-      const current_page = next_page ? page + 1 : null
-      console.log(current_page)
+      const current_page = next_page ? page + 1 : null;
+      console.log(current_page);
       dispatch(listEmployees(current_page));
     }
   };
-
-  useEffect(() => {
-    tbodyRef.current.scrollTop = scrollPos;
-  }, [employee_array]);
 
   // console.log(`page=${page},hasmore->${hasMore},employees->${employee_results && employee_results.length}`)
 
@@ -59,28 +62,32 @@ function TeamTable() {
         className="justify-content-lg-center justify-content-md-center"
       >
         <Col lg="12" md="12">
-        <InfiniteScroll
-      dataLength={employeelist && employeelist.length > 0 ? employeelist.length : null}
-      next={fetchData}
-      hasMore={hasMore}
-      loader={<Loader/>}
-      endMessage={
-        <p style={{textAlign: 'center'}}>
-          <b>Yay! You have seen it all</b>
-        </p>
-      }
-    >
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Designation</th>
-                <th>Date Of Joining</th>
-                <th>Contact</th>
-                <th>Email</th>
-              </tr>
-            </thead>
-              <tbody ref={tbodyRef}>
+          <InfiniteScroll
+            dataLength={
+              employeelist && employeelist.length > 0
+                ? employeelist.length
+                : null
+            }
+            next={fetchData}
+            hasMore={hasMore}
+            loader={<Loader />}
+            endMessage={
+              <p style={{ textAlign: "center" }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }
+          >
+            <MDBTable align="middle">
+              <MDBTableHead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Date Of Joining</th>
+                  <th scope="col">Contact</th>
+                </tr>
+              </MDBTableHead>
+              <MDBTableBody>
                 {loading ? (
                   <Loader />
                 ) : error ? (
@@ -90,11 +97,37 @@ function TeamTable() {
                 ) : employeelist ? (
                   employeelist.map((employee) => (
                     <tr key={employee.id}>
-                      <td>{employee.name}</td>
-                      <td>{employee.designation}</td>
+                      <td>
+                        <div className="d-flex align-items-center">
+                          <img
+                            src={`${baseURL}${employee.profile_image}`}
+                            alt=""
+                            style={{ width: "45px", height: "45px" }}
+                            className="rounded-circle"
+                          />
+                          <div className="ms-3">
+                            <p className="fw-bold mb-1">{employee.name}</p>
+                            <p className="text-muted mb-0">{employee.email}</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p className="fw-normal mb-1">{employee.designation}</p>
+                        <p className="text-muted mb-0">{employee.location}</p>
+                      </td>
+                      <td>
+                        {employee && employee.status === true ? (
+                          <MDBBadge color="success" pill>
+                            Active
+                          </MDBBadge>
+                        ) : (
+                          <MDBBadge color="primary" pill>
+                            OnBoarding
+                          </MDBBadge>
+                        )}
+                      </td>
                       <td>{employee.date_of_joining}</td>
                       <td>{employee.contact_no}</td>
-                      <td>{employee.email}</td>
                     </tr>
                   ))
                 ) : (
@@ -102,8 +135,8 @@ function TeamTable() {
                     Something Wrong Admin To The Rescue
                   </Message>
                 )}
-              </tbody>
-          </Table>
+              </MDBTableBody>
+            </MDBTable>
           </InfiniteScroll>
         </Col>
       </Row>
