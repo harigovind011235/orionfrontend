e9fc44e6833b66b70ccd73295ae479bfa7ca06c7
prema3labglo <prema.labglo@gmail.com>
2023-02-15 10:06:41 +0530

    Admin search pendingleaves

--- src/screens/AdminPendingLeaves.js
+++ src/screens/AdminPendingLeaves.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Table from "react-bootstrap/Table";
 import { useNavigate } from "react-router-dom";
@@ -11,10 +11,27 @@ import Message from "../components/Message";
 function AdminPendingLeaves() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [leavesList, setLeavesList] = useState([]);
   const employeesPendingLeaves = useSelector(
     (state) => state.adminPendingLeaves
   );
   const { error, loading, pendingleaves } = employeesPendingLeaves;
+
+  const handleSearchFilter = (e) => {
+    if (e.target.value !== "") {
+      const filteredValues =
+        leavesList &&
+        leavesList.filter((item) =>
+          item["employee_name"]
+            .toLowerCase()
+            .startsWith(e.target.value.toLowerCase())
+        );
+      setLeavesList(filteredValues);
+    } else {
+      setLeavesList(pendingleaves);
+    }
+  };
+
   useEffect(() => {
     const userData = localStorage.getItem("userInfo");
     if (!userData) {
@@ -23,35 +40,73 @@ function AdminPendingLeaves() {
     dispatch(getPendingLeaves());
   }, [navigate]);
 
+  useEffect(() => {
+    setLeavesList(pendingleaves.sort((element1, element2) => {
+      return element1.employee_name >= element2.employee_name ? 1 : -1;
+    }));
+  }, [employeesPendingLeaves]);
+
   return (
     <>
       <Header />
-      <Table striped bordered hover style={{ marginTop: "100px" }}>
-        <thead>
-          <tr>
-            <th>Employee</th>
-            <th>Pending Requests</th>
-          </tr>
-        </thead>
-        <tbody>
-          {loading ? (
-            <Loader />
-          ) : error ? (
-            <Message variant="danger"></Message>
-          ) : pendingleaves ? (
-            pendingleaves.map((each) => (
+      <br />
+      <div class="row height d-flex justify-content-center align-items-center">
+        <div
+          class="input-group"
+          style={{ maxWidth: "300px", marginLeft: "730px" }}
+        >
+          <input
+            type="search"
+            class="form-control rounded border border-dark"
+            placeholder="Search Employee Name"
+            aria-label="Search"
+            aria-describedby="search-addon"
+            onChange={handleSearchFilter}
+          />
+          &nbsp;&nbsp;
+        </div>
+      </div>
+      <div>
+        <center>
+          <Table
+            striped
+            bordered
+            hover
+            style={{ maxWidth: "1000px", marginTop: "20px" }}
+          >
+            <thead class="table-dark">
               <tr>
-                <td>
-                  <Link to={`/employee-pendingleaves/${each.employee_id}`} style={{ textDecoration: 'none' }}>
-                    {each.employee_name}
-                  </Link>
-                </td>
-                <td>{each.count}</td>
+                <th>Employee</th>
+                <th>Pending Requests</th>
               </tr>
-            ))
-          ) : null}
-        </tbody>
-      </Table>
+            </thead>
+            <tbody>
+              {loading ? (
+                <Loader />
+              ) : error ? (
+                <Message variant="danger"></Message>
+              ) : leavesList.length !== 0 ? (
+                leavesList.map((each) => (
+                  <tr>
+                    <td>
+                      <Link
+                        to={`/employee-pendingleaves/${each.employee_id}`}
+                        style={{ textDecoration: "none" }}
+                        class="text-capitalize"
+                      >
+                        {each.employee_name}
+                      </Link>
+                    </td>
+                    <td>{each.count}</td>
+                  </tr>
+                ))
+              ) : <Message variant="info">
+                Employee not found!
+                </Message>}
+            </tbody>
+          </Table>
+        </center>
+      </div>
     </>
   );
 }
